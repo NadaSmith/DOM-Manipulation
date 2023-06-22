93bfc5a2c98ffbe44da96a9aec04587d8c4cfefc
Nada Smith <103263245+NadaSmith@users.noreply.github.com>
2023-06-22 00:09:50 -0400

    Update script.js
    Write a JavaScript statement that selects the #main-titleID element.  Change the text of the title to something shorter.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+/* Part 1
+DOM's personal website title is a bit wordy. Write a JavaScript statement that selects 
+the #main-titleID element. Remember there are a couple of ways to query id. Change the 
+text of the title to something shorter. */
+const mainTitle = document.querySelector('#main-title');
+mainTitle.textContent = 'Shorter Title';
 
